fdffe247564264027bc13114c37f9c6ec74955e2
Alex Liebrich <alexanderliebrich@web.de>
2023-10-06 12:06:10 +0200

    status with primevue message comp

--- src/components/OkOrError.vue
+++ src/components/OkOrError.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
+import Message from 'primevue/message';
 //import { errorHelper } from '../modules/error';
 //const { error_msg, ok_msg } = errorHelper()
 
@@ -9,15 +10,27 @@ defineProps({
 })
 </script>
 <template>
-    <div class="border-1 surface-border border-rounded">
-    <div class="bg-pink-400 h-4rem border-1 surface-border border-rounded my-1"
+    <div class="border-1 surface-border border-rounded my-1">
+    <Message
+        severity="error"
+        v-if="error_msg">
+        {{ error_msg }}
+    </Message>
+    <Message
+        severity="success"
+        v-if="ok_msg">
+        {{ ok_msg }}
+    </Message>
+    <!--
+    <div class="bg-pink-400 h-4rem "
         v-if="error_msg">
         <div class="px-3 py-3"><span>{{ error_msg }}</span></div>
     </div>
-    <div class="bg-green-500 h-3rem border-1 surface-border border-rounded my-1"
+    <div class="bg-green-500 h-3rem "
         v-if="ok_msg">
         <div class="px-3 py-3"><span>{{ ok_msg }}</span></div>
         
     </div>
+    -->
 </div>
 </template>
